f2d1b9f3f2017ec3ef5a0e19cbfff6cbe05ba923
Jérémy Simar <simar.jeremy@live.fr>
2020-04-08 09:53:30 +0200

    fix: add unique on username column

--- back/src/main/java/com/exasky/dnd/user/model/DnDUser.java
+++ back/src/main/java/com/exasky/dnd/user/model/DnDUser.java
@@ -18,7 +18,7 @@ public class DnDUser implements UserDetails {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column
+    @Column(unique = true)
     private String username;
 
     @Column
