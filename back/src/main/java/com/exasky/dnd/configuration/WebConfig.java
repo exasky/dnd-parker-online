0509f2677110da9865e73887d9ff1e9e9190761d
Jérémy Simar <simar.jeremy@live.fr>
2020-04-09 13:28:05 +0200

    feat: add cache control on images

--- back/src/main/java/com/exasky/dnd/configuration/WebConfig.java
+++ back/src/main/java/com/exasky/dnd/configuration/WebConfig.java
@@ -2,18 +2,23 @@ package com.exasky.dnd.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 import org.springframework.core.io.Resource;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/assets/**/*")
+                .addResourceLocations("classpath:/static/assets/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS));
 
         registry.addResourceHandler("/**/*")
                 .addResourceLocations("classpath:/static/")
