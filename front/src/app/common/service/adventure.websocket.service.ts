d40ad3c7259c9fc4b1bad0cecddc69da62d96a69
Jérémy Simar <simar.jeremy@live.fr>
2020-04-09 13:27:50 +0200

    fix: websocket localhost

--- front/src/app/common/service/adventure.websocket.service.ts
+++ front/src/app/common/service/adventure.websocket.service.ts
@@ -1,11 +1,11 @@
-import { Injectable } from '@angular/core';
-import { InjectableRxStompConfig, RxStompService } from '@stomp/ng2-stompjs';
-import { WebSocketService } from './websocket.service';
-import { WebSocketOptions } from '../model';
+import {Injectable} from '@angular/core';
+import {InjectableRxStompConfig, RxStompService} from '@stomp/ng2-stompjs';
+import {WebSocketService} from './websocket.service';
+import {WebSocketOptions} from '../model';
 
 export const progressStompConfig: InjectableRxStompConfig = {
   webSocketFactory: () => {
-    return new WebSocket('ws://localhost:8080/stomp');
+    return new WebSocket('ws://' + location.hostname + ':8080/stomp');
   }
 };
 
