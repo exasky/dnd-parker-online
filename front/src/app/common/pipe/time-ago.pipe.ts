6ec6f48c887996a57f0b638bbf26bf12c788611b
Jérémy Simar <simar.jeremy@live.fr>
2020-05-05 18:17:54 +0200

    refacto: remove useless code in time pipe

--- front/src/app/common/pipe/time-ago.pipe.ts
+++ front/src/app/common/pipe/time-ago.pipe.ts
@@ -1,39 +1,23 @@
 // https://github.com/AndrewPoyntz/time-ago-pipe
 
-import {Pipe, PipeTransform, NgZone, ChangeDetectorRef, OnDestroy} from "@angular/core";
+import {Pipe, PipeTransform} from "@angular/core";
 import {TranslateService} from "@ngx-translate/core";
 
 @Pipe({
-  name: 'timeAgo',
-  pure: false
+  name: 'timeAgo'
 })
-export class TimeAgoPipe implements PipeTransform, OnDestroy {
-  private timer: number;
-
-  constructor(private changeDetectorRef: ChangeDetectorRef,
-              private ngZone: NgZone,
-              private translate: TranslateService) {
+export class TimeAgoPipe implements PipeTransform {
+  constructor(private translate: TranslateService) {
   }
 
   transform(value: string) {
-    this.removeTimer();
-    let d = new Date(value);
-    let now = new Date();
-    let seconds = Math.round(Math.abs((now.getTime() - d.getTime()) / 1000));
-    let timeToUpdate = (Number.isNaN(seconds)) ? 1000 : this.getSecondsUntilUpdate(seconds) * 1000;
-    this.timer = this.ngZone.runOutsideAngular(() => {
-      if (typeof window !== 'undefined') {
-        return window.setTimeout(() => {
-          this.ngZone.run(() => this.changeDetectorRef.markForCheck());
-        }, timeToUpdate);
-      }
-      return null;
-    });
+    let seconds = Math.round(Math.abs((new Date().getTime() - new Date(value).getTime()) / 1000));
     let minutes = Math.round(Math.abs(seconds / 60));
     let hours = Math.round(Math.abs(minutes / 60));
     let days = Math.round(Math.abs(hours / 24));
     let months = Math.round(Math.abs(days / 30.416));
     let years = Math.round(Math.abs(days / 365));
+
     if (Number.isNaN(seconds)) {
       return '';
     } else if (seconds <= 45) {
@@ -58,32 +42,6 @@ export class TimeAgoPipe implements PipeTransform, OnDestroy {
       return 'a year ago';
     } else { // (days > 545)
       return years + ' years ago';
-    }
-  }
-
-  ngOnDestroy(): void {
-    this.removeTimer();
-  }
-
-  private removeTimer() {
-    if (this.timer) {
-      window.clearTimeout(this.timer);
-      this.timer = null;
-    }
-  }
-
-  private getSecondsUntilUpdate(seconds: number) {
-    let min = 60;
-    let hr = min * 60;
-    let day = hr * 24;
-    if (seconds < min) { // less than 1 min, update every 2 secs
-      return 2;
-    } else if (seconds < hr) { // less than an hour, update every 30 secs
-      return 30;
-    } else if (seconds < day) { // less then a day, update every 5 mins
-      return 300;
-    } else { // update every hour
-      return 3600;
     }
   }
 }
