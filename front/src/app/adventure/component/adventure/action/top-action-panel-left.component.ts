cb4205e41b8334f5b9dbd1869f445582d5970499
Jérémy Simar <simar.jeremy@live.fr>
2021-04-19 08:02:54 +0200

    fix: char unsort on top & endTurn button enabled when no initiative

--- front/src/app/adventure/component/adventure/action/top-action-panel-left.component.ts
+++ front/src/app/adventure/component/adventure/action/top-action-panel-left.component.ts
@@ -1,6 +1,6 @@
-import {Component, Input} from "@angular/core";
-import {Adventure, Initiative} from "../../../model/adventure";
-import {CharacterItem} from "../../../model/item";
+import {ChangeDetectorRef, Component, Input} from "@angular/core";
+import {Initiative} from "../../../model/adventure";
+import {CharacterLayerGridsterItem} from "../../../model/layer-gridster-item";
 
 @Component({
   selector: 'app-top-action-panel-left',
@@ -8,12 +8,22 @@ import {CharacterItem} from "../../../model/item";
   styleUrls: ['./top-action-panel-left.component.scss']
 })
 export class TopActionPanelLeftComponent {
+  sortedCharacterItems: CharacterLayerGridsterItem[] = [];
+
   @Input()
-  characterItems: CharacterItem[] = [];
+  set characterItems(characterItems: CharacterLayerGridsterItem[]) {
+    this.sortCharacters(characterItems);
+  }
 
   @Input()
   currentInitiative: Initiative;
 
-  constructor() {
+  constructor(private cdr: ChangeDetectorRef) {
+  }
+
+  private sortCharacters(characterItems: CharacterLayerGridsterItem[]) {
+    this.sortedCharacterItems = characterItems.filter(c => c.id !== null);
+    this.sortedCharacterItems.sort((c1, c2) => c1.character.name < c2.character.name ? -1 : 1);
+    this.cdr.detectChanges();
   }
 }
--- front/src/app/adventure/component/adventure/action/top-action-panel-right.component.ts
+++ front/src/app/adventure/component/adventure/action/top-action-panel-right.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from "@angular/core";
+import {ChangeDetectorRef, Component, Input, OnDestroy, OnInit} from "@angular/core";
 import {AmbientAudioService, AudioService} from "../../../service/audio.service";
 import {DiceService} from "../../../service/dice.service";
 import {Adventure, Initiative} from "../../../model/adventure";
@@ -6,13 +6,14 @@ import {CharacterItem} from "../../../model/item";
 import {Character} from "../../../model/character";
 import {AdventureService} from "../../../service/adventure.service";
 import {AuthService} from "../../../../login/auth.service";
+import {MatDialog} from "@angular/material/dialog";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-top-action-panel-right',
-  templateUrl: './top-action-panel-right.component.html',
-  // styleUrls: ['./top-action-panel-left.component.scss']
+  templateUrl: './top-action-panel-right.component.html'
 })
-export class TopActionPanelRightComponent {
+export class TopActionPanelRightComponent implements OnInit, OnDestroy {
   @Input()
   adventure: Adventure;
 
@@ -23,12 +24,32 @@ export class TopActionPanelRightComponent {
   initiatives: Initiative[] = [];
 
   disableActions: boolean = false;
+  private afterOpenSub: Subscription;
+  private afterCloseSub: Subscription;
 
   constructor(private adventureService: AdventureService,
               private diceService: DiceService,
               public authService: AuthService,
               public audioService: AudioService,
-              public ambientAudioService: AmbientAudioService,) {
+              public ambientAudioService: AmbientAudioService,
+              private dialog: MatDialog,
+              private cdr: ChangeDetectorRef) {
+  }
+
+  ngOnInit() {
+    this.afterOpenSub = this.dialog.afterOpened.subscribe(() => {
+      this.disableActions = true;
+      this.cdr.detectChanges();
+    });
+    this.afterCloseSub = this.dialog.afterAllClosed.subscribe(() => {
+      this.disableActions = false;
+      this.cdr.detectChanges();
+    });
+  }
+
+  ngOnDestroy() {
+    this.afterOpenSub.unsubscribe();
+    this.afterCloseSub.unsubscribe();
   }
 
   rollDices() {
